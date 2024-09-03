52a7432ae3d1796c6eafec48430c1d997e926759
danialvfd <danialvfd@gmail.com>
2024-09-03 07:51:35 +0400

    chore: js example

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,6 +1,22 @@
-var MyButton = document.getElementById("Mybutton");
-MyButton.addEventListener("click",ShowMessage);
+var submitButton = document.getElementById("submitButton");
+var columnBackgroundRed = document.getElementById ("columnBackgroundRed").style.backgroundColor = "red";
+
+submitButton.addEventListener("click",ShowMessage);
  
 function ShowMessage(){
  alert("درخواست شما ثبت شد");
 }
+
+$(document).ready(function(){
+    $("th, td").click(function(){
+      $(this).hide();
+    });
+  });
+
+  $("p").dblclick(function(){
+    $(this).hide();
+  });
+
+  $("#p1").mouseenter(function(){
+    alert("You entered the <p>");
+  });
