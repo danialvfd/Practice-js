bbf865107d2f18615abd0bdcf469182231681628
danialvfd <danialvfd@gmail.com>
2024-09-24 09:02:06 +0400

    chore: mathjs

---
+++ js/testmathjs.js
@@ -0,0 +1,8 @@
+var sumTwoNumber = math.add (10 , 15);
+console.log("جمع دو عدد:" + sumTwoNumber)
+
+var sqrtResult = math.sqrt(25);
+console.log("جذر عدد:" + sqrtResult);
+
+let power = math.pow(3, 4);
+console.log("رساندن یک عدد به توان دلخواه:: " + power);
