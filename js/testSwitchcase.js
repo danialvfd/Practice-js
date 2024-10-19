912253fce2d941196d05a39356b0ed0e82f91d33
danialvfd <danialvfd@gmail.com>
2024-10-19 08:49:54 +0400

    feat: switch cas js

---
+++ js/testSwitchcase.js
@@ -0,0 +1,10 @@
+switch (day) {
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+    default:
+        console.log("Not Monday or Tuesday");
+}
