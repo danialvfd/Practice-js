be2db00532387931597cbd41d4b925a79c013933
danialvfd <danialvfd@gmail.com>
2024-11-02 08:48:05 +0400

    chor: interface

--- ts/test.ts
+++ ts/test.ts
@@ -5,3 +5,21 @@ function greeter(person) {
 let user = "Jane User";
 
 document.body.textContent = greeter(user);
+
+
+interface Book {
+    title: string;
+    author: string;
+    yearPublished: number;
+  }
+
+  let book1: Book = {
+    title: "قلعه حیوانات",
+    author: "جورج اورول",
+    yearPublished: 1945,
+  };
+
+  let book2: Book = {
+    title: "شاهنامه",
+    author: "فردوسی"
+  }
